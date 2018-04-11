063cd1d3b06f0aa55bf4048d550ac0e6ee2c9a1d
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2018-04-11 22:46:59 +0000

    Update conta_letras.py

--- tutoriais/conta_letras.py
+++ tutoriais/conta_letras.py
@@ -53,3 +53,19 @@ def procura_letraS(texto):
       else:
         pass
     return maior[1]
+# -------------------------------------------------------
+#                solution
+# -------------------------------------------------------
+
+import string
+def checkio(text):
+    """
+    We iterate through latyn alphabet and count each letter in the text.
+    Then 'max' selects the most frequent letter.
+    For the case when we have several equal letter,
+    'max' selects the first from they.
+    """
+    text = text.lower()
+    return max(string.ascii_lowercase, key=text.count)
+
+
