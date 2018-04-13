0aa4b926209c3998ab878b75729a8ff8175b9c1a
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2018-04-12 23:52:47 -0300

    Create ruidos.py

---
+++ tutoriais/ruidos.py
@@ -0,0 +1,12 @@
+# Dada uma lista não vazia de inteiros (X). Para esta tarefa, você deve retornar uma lista consistindo apenas de elementos 
+# não únicos nesta lista. Para isso você, você deve remover todos os elementos exclusivos (que estão presentes na lista
+# apenas uma vez). Ao resolver este problema, não altere a ordem original dos elementos da lista. Exemplo: [1, 2, 3, 1, 3] 
+# em que 1 e 3 não são únicos e o resultado será [1, 3, 1, 3].
+
+# Entrada: Uma lista (list) de inteiros (int).
+# Output: A lista (list) de inteiros (int).
+
+# Como é utilizado: Esta missão vai ajudar você a entender como manipular vetores, algo que é a base para a resolução de 
+# tarefas mais complexas. O conceito pode ser facilmente generalizado para problemas do mundo real. Por exemplo: se você 
+# precisa limpar estatísticas removendo elementos de baixa frequência (ruído)
+
